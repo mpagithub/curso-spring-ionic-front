0772b3b5d364bf5afe730a423902dd16644e9e65
Marcelo Almeida <mpaemail@gmail.com>
2020-02-14 18:55:38 -0300

    Aula - 113

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage } from 'ionic-angular';
+import { NavController, IonicPage, MenuController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,26 +8,32 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
 
 }
 
-  // se vc omite o modificador public, por default, já é public
-  // Método de login, que por enquanto vai chamar a página de categoria
-  login() {
-      /* Para chamar categoria, vamos usar o navController que já está injetado logo acima
-         No Ionic e no Angular, quando vc quer injetar uma instância de um objeto, em uma classe,
-         basta vc declarar este objeto como parâmetro do construtor.
+// se vc omite o modificador public, por default, já é public
+// Método de login, que por enquanto vai chamar a página de categoria
+login() {
+   this.navCtrl.setRoot('CategoriasPage');  // vamos usar o setRoot, que abre uma página sem empilhar
+}
+/* Para chamar categoria, vamos usar o navController que já está injetado logo acima
+     No Ionic e no Angular, quando vc quer injetar uma instância de um objeto, em uma classe,
+     basta vc declarar este objeto como parâmetro do construtor.
          
-         o método push, impilha uma página em cima da outra, muito usado em aplicativos de 
-         de dispositivos móveis.
-         this.navCtrl.push('CategoriasPage');
-     */
-     
-     this.navCtrl.setRoot('CategoriasPage');  // vamos usar o setRoot, que abre uma página sem empilhar
-   
-  }
+     o método push, impilha uma página em cima da outra, muito usado em aplicativos de dispositivos móveis.
+     this.navCtrl.push('CategoriasPage');
+*/
  
+ionViewDidLoad() {
+  this.menu.swipeEnable(false);
+}
+ionViewDidLeave() {
+  this.menu.swipeEnable(true);
+}
+/* Aula 113 : Desabilitando o menu na página home. Quando queremos executar uma ação quando entra na página
+   ou quando sai da página, devemos utilizar o “ionic lifecycle events”. Busque isso no google e entre no 
+   link do NavController e procure os eventos. */
 
 
 }
